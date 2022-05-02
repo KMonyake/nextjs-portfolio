55c3c5a21704dd76183ebf01711acb9e90e51959
KMonyake <86020119+KMonyake@users.noreply.github.com>
2022-05-02 19:07:25 +0200

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Script from "next/script";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Script 
+      strategy="lazyOnload"
+        src={"https://www.googletagmanager.com/gtag/js?id=G-GTSMJHETRL"}
+    />
+    <Script strategy="lazyOnload">
+      {
+        `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-GTSMJHETRL');
+        `
+      }
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
