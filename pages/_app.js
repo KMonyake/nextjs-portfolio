b676b958f6148e3c1e212b9f3af54822939af653
KMonyake <86020119+KMonyake@users.noreply.github.com>
2022-05-24 00:41:54 +0200

    removed unused pages

--- pages/_app.js
+++ pages/_app.js
@@ -5,20 +5,22 @@ import { GlobalStyles } from "../styled/global.styled";
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyles />
-      <Script 
+      <GlobalStyles/>
+      <Script
+        id="my-script"
         strategy="lazyOnload"
-          src={"https://www.googletagmanager.com/gtag/js?id=G-GTSMJHETRL"}
+        src={"https://www.googletagmanager.com/gtag/js?id=G-GTSMJHETRL"}
       />
-      <Script id="my-script" strategy="lazyOnload">
+      <Script
+        id="my-script2"
+        strategy="lazyOnload">
         {
           `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-GTSMJHETRL');
-          `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-GTSMJHETRL');
+        `
         }
       </Script>
       <Component {...pageProps} />
